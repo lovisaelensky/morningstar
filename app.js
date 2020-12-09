bc30f86a68c473ef01ce366999ba6acc6c31e377
Lovisa Elensky <lovisa@Lovisas-MacBook-Pro-2.local>
2020-12-10 00:35:16 +0100

    last changes before bed......

--- app.js
+++ app.js
@@ -20,9 +20,26 @@ ScrollReveal().reveal('.hero-text', {
 
 ScrollReveal().reveal('.hero-img', {
   origin: 'right',
-  distance: '1000px',
+  distance: '0px',
   easing: 'ease',
   scale: '0.6',
   reset: false,
   delay: 500,
+});
+
+ScrollReveal().reveal('.img-container', {
+  origin: 'bottom',
+  distance: '100px',
+  easing: 'ease',
+  scale: '0.6',
+  reset: false,
+  delay: 0,
+});
+ScrollReveal().reveal('.cupola-text', {
+  origin: 'left',
+  distance: '1000px',
+  easing: 'ease',
+  scale: '0.6',
+  reset: false,
+  delay: 100,
 });
